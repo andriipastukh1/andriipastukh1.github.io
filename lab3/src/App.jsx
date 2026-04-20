e2477fdc5129b979fd68e965c34ad0545405c47a
AndriyPastukh <vasylpaspuh2@gmail.com>
2026-04-20 10:49:08 +0300

    Fix: removed node_modules from repo

--- lab3/src/App.jsx
+++ lab3/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
 import { Sun, Moon } from 'lucide-react';
 import MyGoals from './pages/MyGoals';
 import ProgressPage from './pages/ProgressPage';
